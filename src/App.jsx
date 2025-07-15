d29a1aea0f75dbefd5813dd9113cdde4534452b3
Ana nobrega <anakarolinefrnobrega@gmail.com>
2025-07-15 14:37:25 -0300

    Atualizações

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,24 @@
-// src/App.jsx
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-
-import Header   from './components/Header.jsx'
-import Footer   from './components/Footer.jsx'
-
+import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
 import Cadastro from './pages/Cadastro.jsx'
-import Triagem  from './pages/Triagem.jsx'
-import Medico   from './pages/Medico.jsx'
-import Painel   from './pages/Painel.jsx'
-
-// ⚠️ Corrigido o caminho para onde seu CSS realmente está
-import './styles/App.css'
+import Triagem from './pages/Triagem.jsx'
+import Medico from './pages/Medico.jsx'
+import Painel from './pages/Painel.jsx'
 
 export default function App() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-
-      <main className="flex-grow">
+      <main className="flex-grow bg-gray-50">
         <Routes>
-          <Route path="/"        element={<Cadastro />} />
-          <Route path="/triagem" element={<Triagem  />} />
-          <Route path="/medico"  element={<Medico   />} />
-          <Route path="/painel"  element={<Painel   />} />
+          <Route path="/" element={<Cadastro />} />
+          <Route path="/triagem" element={<Triagem />} />
+          <Route path="/medico" element={<Medico />} />
+          <Route path="/painel" element={<Painel />} />
         </Routes>
       </main>
-
       <Footer />
     </div>
   )

bdb6609d6b51df68cc051056d0049ef624eaf1c9
gabriel-chagas-albuquerque <gabrieldafortaleza0100@gmail.com>
2025-09-25 14:35:09 -0300

    Conserta problema na autenticação do usuário

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,10 +1,10 @@
-import { FaUserMd, FaExclamationTriangle, FaSignInAlt } from 'react-icons/fa';
+import { FaUserMd, FaSearch} from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
-const NotAuthorization = () => {
+const NotFound = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100 px-4">
       <div className="w-full max-w-md p-8 bg-white rounded-2xl shadow-lg relative">
-        {/* Header com ícone igual ao login */}
         <div className="hospital-logo text-center mb-8">
           <div className="w-16 h-16 rounded-full bg-[#2f6f3d] text-white flex items-center justify-center mx-auto mb-4 shadow-lg">
             <FaUserMd className="text-3xl" />
@@ -15,21 +15,22 @@ const NotAuthorization = () => {
           <div className="hospital-subtitle text-gray-500">Sistema de Gestão Hospitalar</div>
         </div>
 
-        {/* Conteúdo de não autorização */}
         <div className="text-center space-y-6">
-          {/* Ícone de aviso */}
-          <div className="w-20 h-20 rounded-full bg-red-100 text-red-500 flex items-center justify-center mx-auto">
-            <FaExclamationTriangle className="text-4xl" />
+          <div className="w-20 h-20 rounded-full bg-green-100 text-green-800 flex items-center justify-center mx-auto">
+            <FaSearch className="text-4xl" />
           </div>
 
           {/* Mensagem principal */}
           <div>
             <h1 className="text-xl font-bold text-gray-800 mb-2">
-              Acesso Não Autorizado
+              Página Não encontrada
             </h1>
-            <p className="text-gray-600 text-sm">
-              Você precisa fazer login para acessar esta página
-            </p>
+            
+            <Link to={"/"}>
+                <button className="w-full flex items-center justify-center gap-2 bg-gradient-to-r from-[#59995c] to-[#4a8049] text-white py-3 rounded-lg font-semibold hover:opacity-95 transition relative cursor-pointer">
+                  Clique aqui para ir à página de login
+                </button>
+            </Link>
           </div>
         </div>
       </div>
@@ -37,4 +38,4 @@ const NotAuthorization = () => {
   );
 };
 
-export default NotAuthorization;
+export default NotFound;
--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -31,6 +31,7 @@ export const AuthProvider = ({ children }) => {
   const logout = () => {
     setUser(null)
     localStorage.removeItem('user')
+    localStorage.removeItem('token')
   }
 
   const updateUser = (userData) => {
